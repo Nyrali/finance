c97ece777501344e580f12002de7cd02c0a8537c
Ghar <rudolf.tkac@seznam.cz>
2025-06-07 01:48:59 +0200

    black

--- data_processing.py
+++ data_processing.py
@@ -13,24 +13,3 @@ df_filter = df[df["booking_date"] > "2024-01-01"]
 categories = [i for i in df["category_high_lvl"].unique()]
 
 generate_chart_with_templates(df_filter, categories)
-
-
-df = pd.read_csv(
-    "D:/finance/patron.csv",
-    sep=";",
-)
-
-df = patron_data_cleanse(df)
-df["category_high_lvl"] = (
-    df["category_high_lvl"]
-    .fillna("Jiné finanční výdaje")  # skutečné NaN
-    .astype(str)
-    .replace(
-        ["nan", "NaN", "None", ""], "Jiné finanční výdaje"
-    )  # stringové "neviditelné" NaN
-    .str.strip()
-)
-
-
-categories = [i for i in df["category_high_lvl"].unique()]
-patron_generate_resizable_stacked_chart_html_1(df, categories)
